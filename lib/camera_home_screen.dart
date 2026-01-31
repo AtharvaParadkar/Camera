7431c2e67b2bf64de3539abe91a09d3d9c9c6952
AtharvaParadkar <atharvaparadkar91@gmail.com>
2026-01-31 19:24:16 +0530

    Fetch available cameras

---
+++ lib/camera_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CameraHomeScreen extends StatelessWidget {
+  const CameraHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('data')));
+  }
+}
